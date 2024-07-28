e5136618ee7c0ffb631b07c8cf0f5fbdb1c2c080
baenary <150470877+kpdl0527@users.noreply.github.com>
2024-07-28 17:48:36 +0000

    updater.py structure

--- src/updater/updater.py
+++ src/updater/updater.py
@@ -1,3 +1,16 @@
-# the updater executable will
+# the updater executable will sit next to yt_clipper
 
 # get version of installed yt_clipper version on users local machine
+
+def get_installed_version() : 
+    version_number = ""
+    return
+
+def get_latest_version() : 
+    version_number = ""
+    return
+
+def update(installed_version, lastest_version) :
+    if (lastest_version > installed_version) :
+        # update
+        return
